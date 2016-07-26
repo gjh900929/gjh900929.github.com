49ccddc66ffc609859a630a53bbf0ee028592a93
guojiahuan <396352666@qq.com>
2016-07-26 15:09:08 +0800

    534

---
+++ qcdx/js/index.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(function(){
+	$(".nav a").click(function(){
+		$(this).css("background","#0c71bc");
+	})
+		
+		
+	$(".fot_im").eq(1).css("padding-top",".4em");
+	$(".fot_im img:last").css("width","85%");
+		
+		
+	
+})
