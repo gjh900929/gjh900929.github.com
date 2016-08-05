85820ac8ae52047d73c2d6baae1e488d06ca3680
guojiahuan <396352666@qq.com>
2016-08-05 10:45:26 +0800

    1232

---
+++ 0805.01/js/right_xin.js
@@ -0,0 +1,8 @@
+document.writeln("<style type=\"text/css\">");
+document.writeln(".pf10{position:fixed !important; _position:absolute; _bottom:auto; _top:expression(eval(document.documentElement.scrollTop+document.documentElement.clientHeight-this.offsetHeight-(parseInt(this.currentStyle.marginTop,10)||0)-(parseInt(this.currentStyle.marginBottom,10)||0))); top:34%; margin-bottom:120px; right:3px; z-index:999;}");
+
+document.writeln("</style>");
+document.writeln("<div class=\"pf10\">");
+document.writeln("<a target=\"_blank\" href=\"tel:085185716090\" style=\"cursor:pointer\">");
+document.writeln("<img src=\"/swt/zxzx.gif\"></a>");
+document.writeln("</div>");
