36ec7a7f8a3643dca5fb8f6947e7767d21780596
guojiahuan <396352666@qq.com>
2016-09-02 10:53:13 +0800

    del

--- 07-04.1/js/tab.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".part1-hd li").click(function(){
-        $(this).addClass("on").siblings().removeClass("on");
-
-        $(".part1-bd>div").eq($('.part1-hd li').index(this)).show().siblings().hide();
-    })
-})
